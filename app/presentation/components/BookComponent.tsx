0bb4c23e8d1027b504ed20dbf6b11db494f85c91
zhoque <zhoque@costar.com>
2024-01-24 15:33:05 -0800

    sample test run

---
+++ app/presentation/components/BookComponent.tsx
@@ -0,0 +1,22 @@
+
+import React, {useState} from 'react';
+import {Text, View, Button} from 'react-native';
+import getSampleBookData from "./BookPresenter";
+
+const BookComponent = () => {
+    const [bookName, setBookName] = useState('Untitled');
+
+    const getSampleBookDataHandler = () => {
+        const book = getSampleBookData();
+        setBookName(book.title);
+    };
+
+    return (
+        <View>
+            <Button title="Get Sample Book" onPress={getSampleBookDataHandler} />
+            <Text>{bookName}</Text>
+        </View>
+    );
+}
+
+export default BookComponent;
