da527cf2acf8c2a4feb5d379fce9910e86e3a084
zhoque <zhoque@costar.com>
2024-07-18 23:19:00 -0700

    removed authentication for now

--- app/contexts/userContext.ts
+++
@@ -1,7 +0,0 @@
-import React, {Context} from 'react';
-import {AuthorizeResult, RefreshResult} from 'react-native-app-auth';
-
-export const UserContext = React.createContext<any>({
-    user: null,
-    updateUser: (user: AuthorizeResult | RefreshResult | null) => {},
-});
--- app/presentation/navigation/bottom-tab-nav.tsx
+++ app/presentation/navigation/bottom-tab-nav.tsx
@@ -4,14 +4,12 @@ import {BottomNavigation, BottomNavigationTab, Icon, useTheme} from "@ui-kitten/
 import t from "../../i18n/18n";
 import {ScreenANav} from "./screen-a-nav";
 import React from "react";
-import {UserContext} from "../../contexts/userContext";
 
 
 const BottomTabs = createBottomTabNavigator();
 
 const BottomTabNav = () => {
     const theme = useTheme();
-    const {user} = React.useContext(UserContext);
 
     const geIcon = (name: string) => <Icon name={name} style={{ width: 18, height: 18 }} />;
 
